7343da9c444ad9ce08bddb5239245d422d471bd4
杉田親次朗 <ba0534ps@icloud.com>
2019-12-09 03:10:46 +0900

    feat: Nice comment style

---
+++ initialize.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+echo 'HELLO! from  initialize.sh'
+cd ~
+
+#--------------------------------------------------------------#
+##        OS settings                                         ##
+#--------------------------------------------------------------#
+echo 'start: setup os settings'
+defaults write com.apple.finder AppleShowAllFiles TRUE
+echo 'complete: setup os settings'
+
+#--------------------------------------------------------------#
+##        HomeBrew                                            ##
+#--------------------------------------------------------------#
+echo 'start: Install HomeBrew'
+/usr/bin/ruby -e "$(/usr/bin/curl -fksSL https://raw.github.com/mxcl/homebrew/master/Library/Contributions/install_homebrew.rb)"
+brew update
+brew doctor
+echo 'complete: Install Homebrew'
+
+echo 'Installing git..'
+brew install git
+
+echo 'Installing openssl..'
+brew install openssl
+
+#--------------------------------------------------------------#
+##        Clean setting files                                 ##
+#--------------------------------------------------------------#
+echo 'start: Clean setting files'
+rm ~/.zshrc
+rm ~/.zprofile
+rm ~/.bashrc
+rm ~/.bash_profile
+rm ~/.vimrc
+rm -r ~/.vim
+echo 'complete: Clean setting files'
+
+#--------------------------------------------------------------#
+##        set Symbolic Links                                  ##
+#--------------------------------------------------------------#
+echo 'start: setup Symbolic Links'
+ln -s ~/dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.zprofile ~/.zprofile
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/.vim ~/.vim 
+ln -s ~/dotfiles/.dein ~/.dein
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.tigrc ~/.tigrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/alacritty.yml ~/.config/alacritty/alacritty.yml
+source ~/.bash_profile
+source ~/.zshrc
+echo 'complete: setup Symbolic Links'
